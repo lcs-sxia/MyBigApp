a5134ecede2e87a19866d348c1e9f1847e7ea5da
Steven Xia <sxia@lcs.on.ca>
2026-03-01 14:19:44 -0500

    Got the "Favourites" part done by using custom subview.

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack{
-            VStack{
+            VStack(spacing:20){
+                
                 VStack(spacing: 20){
                     Text("Waste no more time arguing about what a good man should be. Be one.")
                         .multilineTextAlignment(.center)
@@ -32,10 +33,19 @@ struct ContentView: View {
                         .frame(width:30)
                         .foregroundColor(.gray)
                 }
-                Spacer()
                 Label: do{
                     Text("Another one!")
                         .foregroundColor(.blue)
+                }
+                HStack{
+                    Text("Favourites")
+                        .font(Font.title.weight(.black))
+                    Spacer()
+                }
+                VStack{
+                    FavouriteTextView(quotes: "Kind words can be shout and easy to speak but their echoes are truly endless.")
+                    FavouriteTextView(quotes: "Anticipate the difficult things by managing the easy.")
+                    FavouriteTextView(quotes: "I have an everyday religion that works for me. Love yourself first, and everything else falls into line.")
                 }
             
             }
@@ -47,4 +57,14 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct FavouriteTextView: View {
+    let quotes: String
+    var body: some View {
+        Text(quotes)
+            .padding()
+            .lineSpacing(5.0)
+        
+    }
 }
