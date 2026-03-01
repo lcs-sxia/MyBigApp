8b3519b28984197186222714b13f453c3615ef24
Steven Xia <sxia@lcs.on.ca>
2026-03-01 14:04:57 -0500

    Finish the quotes part before favourtes

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                VStack(spacing: 20){
+                    Text("Waste no more time arguing about what a good man should be. Be one.")
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .padding(.horizontal)
+                    HStack{
+                        Spacer()
+                        Text("- Marcus Aurelius")
+                            .italic()
+                            .padding(.horizontal)
+                    }
+                }
+                .lineSpacing(5.0)
+                .border(.black)
+                Label: do {
+                    Image(systemName:"heart.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:30)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                Label: do{
+                    Text("Another one!")
+                        .foregroundColor(.blue)
+                }
+            
+            }
+            .padding()
+            .navigationTitle("Quotes")
         }
-        .padding()
     }
 }
 
