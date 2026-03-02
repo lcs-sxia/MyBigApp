86c7967f6cc79e015b4fcf04377fda61e7057bf4
Steven Xia <sxia@lcs.on.ca>
2026-03-02 11:16:15 -0500

    ContentView2 file is created.

---
+++ MyBigApp/ContentView2.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView2.swift
+//  MyBigApp
+//
+//  Created by Steven Xia on 2026/3/2.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView2()
+}
