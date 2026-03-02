313cbbf703a585335cc3bf40ea3bec1b65bc45d2
Steven Xia <sxia@lcs.on.ca>
2026-03-02 11:33:33 -0500

    primaryly finish the supply View part.

--- MyBigApp/ContentView2.swift
+++ MyBigApp/ContentView2.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 struct ContentView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                HStack{
+                    Text("Supply")
+                        .font(.title2)
+                    Spacer()
+                    Label: do {
+                        Image(systemName:"plus.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:25)
+                            .foregroundColor(.black)
+                    }
+                }
+                
+                
+            }
+            .navigationTitle("Supply")
+            .background(RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.gray))
+        }
+        
     }
 }
 
 #Preview {
     ContentView2()
+}
+
+struct SupplyView: View {
+    let name: String
+    let lastPurchaseDate: String
+    let needForSupplyOrNot: String
+    var body: some View{
+        HStack{
+            Rectangle()
+                .frame(width:100, height:150)
+            VStack{
+                Text(name)
+                    
+                Text(lastPurchaseDate)
+                    .foregroundColor(.black)
+                Text(needForSupplyOrNot)
+                    
+            }
+        }
+    
+    }
 }
