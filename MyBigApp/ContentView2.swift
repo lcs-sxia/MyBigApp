b14a6f647c2be854db63f485095eba637d81ce0b
Steven Xia <sxia@lcs.on.ca>
2026-03-02 12:00:55 -0500

    Finish everything except the bottom tab part.

--- MyBigApp/ContentView2.swift
+++ MyBigApp/ContentView2.swift
@@ -13,7 +13,7 @@ struct ContentView2: View {
             VStack{
                 HStack{
                     Text("Supply")
-                        .font(.title2)
+                        .font(.largeTitle)
                     Spacer()
                     Label: do {
                         Image(systemName:"plus.circle")
@@ -23,10 +23,15 @@ struct ContentView2: View {
                             .foregroundColor(.black)
                     }
                 }
+                SupplyView(name: "Milk", lastPurchaseDate: "Dec.11", needForSupplyOrNot: "No need for supply")
+                SupplyView(name: "Meat", lastPurchaseDate: "Dec.5", needForSupplyOrNot: "Need for supply")
+                SupplyView(name: "Vegetable&Fruit", lastPurchaseDate: "Dec.9", needForSupplyOrNot: "No need for supply")
+                SupplyView(name: "Egg", lastPurchaseDate: "Dec.6", needForSupplyOrNot: "Need for supply")
                 
                 
             }
             .navigationTitle("Supply")
+            .padding(.horizontal)
             .background(RoundedRectangle(cornerRadius: 10)
             .foregroundColor(.gray))
         }
@@ -45,15 +50,28 @@ struct SupplyView: View {
     var body: some View{
         HStack{
             Rectangle()
-                .frame(width:100, height:150)
+                .frame(width:90, height:120)
+            Spacer()
             VStack{
-                Text(name)
-                    
-                Text(lastPurchaseDate)
-                    .foregroundColor(.black)
-                Text(needForSupplyOrNot)
-                    
+                HStack{
+                    Text(name)
+                        .fontWeight(.heavy)
+                        .font(.title2)
+                    Spacer()
+                }
+                HStack{
+                    Text("Last purchase: \(lastPurchaseDate)")
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+                HStack{
+                    Text(needForSupplyOrNot)
+                    Spacer()
+                }
             }
+            Spacer()
+            Text("\(Image(systemName: "chevron.right"))")
+                .foregroundStyle(.black)
         }
     
     }
